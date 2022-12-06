674345b1559af9db9b3844ced8baa3a81243b2a1
Kody Horiuchi <kody.a.horiuchi@gmail.com>
2022-12-06 09:49:24 -0500

    added template and homepage

--- app.py
+++ app.py
@@ -7,3 +7,14 @@ app.debug = True
 
 boggle_game = Boggle()
 
+
+@app.route("/")
+def homepage():
+    """Makes the board"""
+
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    number_plays = session.get("number_plays", 0)
+
+    return render_template("index.html", board=board, highscore=highscore, number_plays=number_plays)
