9bb738ca0014fed576fc7d1a65d55f205811439f
Kody Horiuchi <kody.a.horiuchi@gmail.com>
2022-12-06 09:33:00 -0500

    added flask imports

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+from flask import Flask, request, render_template, redirect, session
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "myboggle123"
+app.debug = True
+
 boggle_game = Boggle()
+
