b6e1ef2733309ce188743ee2f8af4d365fba9df8
Kody Horiuchi <kody.a.horiuchi@gmail.com>
2022-12-06 16:42:52 -0500

    added static folder

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request, render_template, redirect, session, jsonify
 from boggle import Boggle
 
 app = Flask(__name__)
@@ -18,3 +18,14 @@ def homepage():
     number_plays = session.get("number_plays", 0)
 
     return render_template("index.html", board=board, highscore=highscore, number_plays=number_plays)
+
+
+@app.route("/check-word")
+def check_word():
+    """Checks the word to see if it's valid"""
+
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({"result": response})
