eba3271420b35b109a5ec227da17922629e976f4
Kody Horiuchi <kody.a.horiuchi@gmail.com>
2022-12-07 13:00:23 -0500

    added unittests

--- test.py
+++ test.py
@@ -6,5 +6,30 @@ from boggle import Boggle
 
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
+    def setUp(self):
+        self.client = app.test_client()
+        app.config['TESTING'] = True
 
+    def test_homepage(self):
+        """Testing that the information displayed is correct"""
+
+        with self.client:
+            response = self.client.get("/")
+            self.assertIn('board', session)
+            self.assertIsNone(session.get('highscore'))
+            self.assertIsNone(session.get('number_plays'))
+            self.assertIn(b'<p>High Score', response.data)
+
+    def test_invalid_word(self):
+        """Test if the word is on the board"""
+
+        self.client.get('/')
+        response = self.client.get('/check-word?word=freakish')
+        self.assertEqual(response.json['result'], 'not-on-board')
+
+    def test_not_real_word(self):
+        """Test if the word is in the dictionary"""
+
+        self.client.get('/')
+        response = self.client.get('/check-word?word=adsfjasdfl')
+        self.assertEqual(response.json['result'], 'not-word')
